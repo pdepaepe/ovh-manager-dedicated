0bf0eea81807d0b62240cbbe98bfc5cd1fd8e970
Cyrille Bourgois <cyrille.bourgois@corp.ovh.com>
2018-11-19 11:07:35 +0100

    refactor(components.contract): move to dedicatedUniverseComponent

--- client/app/dedicatedUniverseComponents/index.js
+++ client/app/dedicatedUniverseComponents/index.js
@@ -1,9 +1,12 @@
 import angular from 'angular';
 
+import ducContract from './contract';
+
 const moduleName = 'dedicatedUniverseComponents';
 
 angular
   .module(moduleName, [
+    ducContract,
   ]);
 
 export default moduleName;
